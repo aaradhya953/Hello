73ef0992b2dabcf0b83ad21c3a1f33427b4facfb
aaradhya953 <aaradhya.953@fapment.com>
2025-04-24 00:38:15 +0700

    Update 99.php

--- 99.php
+++ 99.php
@@ -1,6 +1,6 @@
 <?php
 define('host',['99MakeMoneyonline','99makemoneyonline.com','']);
-define('version','1.0');
+define('version','1.0.1');
 define('cok','cookie.'.host[0]);
 define('uag','user_agent');
 define('web','https://'.host[1]);
